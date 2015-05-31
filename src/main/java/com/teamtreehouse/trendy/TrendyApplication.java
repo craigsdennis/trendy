390efe47d37c96f604fc6791f793966e444d0838
Craig Dennis <craig@teamtreehouse.com>
2015-05-30 21:14:59 -0700

    Initial import of starter.spring.io.

---
+++ src/main/java/com/teamtreehouse/trendy/TrendyApplication.java
@@ -0,0 +1,12 @@
+package com.teamtreehouse.trendy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrendyApplication.class, args);
+    }
+}
