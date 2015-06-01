a8131b40f1f76085066cbf7b58fc98f79d24d237
Craig Dennis <craig@teamtreehouse.com>
2015-05-31 22:35:13 -0700

    Adds temporary web home

---
+++ src/main/java/com/teamtreehouse/trendy/web/HomeController.java
@@ -0,0 +1,16 @@
+package com.teamtreehouse.trendy.web;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableAutoConfiguration
+public class HomeController {
+
+    @RequestMapping("/")
+    String index(Model model) {
+        return "index";
+    }
+}
